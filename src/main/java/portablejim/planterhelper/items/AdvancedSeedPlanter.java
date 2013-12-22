b6d4f95ab52917e57065f5472273422e2582aa66
James McKay <dcseee-github@yahoo.com.au>
2013-12-23 02:24:44 +1100

    Add planting features to advanced planter

--- src/main/java/portablejim/planterhelper/items/AdvancedSeedPlanter.java
+++ src/main/java/portablejim/planterhelper/items/AdvancedSeedPlanter.java
@@ -5,8 +5,12 @@ import net.minecraft.creativetab.CreativeTabs;
 import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemStack;
+import net.minecraft.util.MathHelper;
 import net.minecraft.world.World;
+import net.minecraftforge.common.ForgeDirection;
 import portablejim.planterhelper.PlanterHelper;
+import portablejim.planterhelper.core.PlantingLogic;
+import portablejim.planterhelper.core.PlantingUtil;
 import portablejim.planterhelper.inventories.SmallInventory;
 import portablejim.planterhelper.items.lib.InventoryPlanterHelperTool;
 
@@ -32,12 +36,31 @@ public class AdvancedSeedPlanter extends InventoryPlanterHelperTool {
     @Override
     public ItemStack onItemRightClick(ItemStack par1ItemStack, World par2World, EntityPlayer par3EntityPlayer) {
 
-        if(!par2World.isRemote) {
-            inventory.loadFromNBT(par1ItemStack);
+        if(par3EntityPlayer.isSneaking()) {
+            if(!par2World.isRemote) {
+                inventory.loadFromNBT(par1ItemStack);
+            }
+            par3EntityPlayer.openGui(PlanterHelper.instance, 0, par2World, 0, 0, 0);
         }
-        par3EntityPlayer.openGui(PlanterHelper.instance, 0, par2World, 0, 0, 0);
 
         return par1ItemStack;
+    }
+
+    @Override
+    public boolean onItemUse(ItemStack itemStackUsed, EntityPlayer player, World world, int x, int y, int z, int intDirection, float par8, float par9, float par10) {
+        if(!player.isSneaking()) {
+            int intFacing = MathHelper.floor_double((double) (player.rotationYaw * 4.0F / 360.0F) + 0.5D) & 3;
+            ForgeDirection[] directions = { ForgeDirection.SOUTH, ForgeDirection.WEST, ForgeDirection.NORTH, ForgeDirection.EAST };
+            if(PlantingUtil.canPlant(player.inventory, world, x, y, z, ForgeDirection.getOrientation(intDirection))) {
+                ForgeDirection direction = directions[intFacing];
+                PlantingLogic.plantSquare(player.inventory, world, x, y, z, 9, direction, PlantingUtil.getTargetSlot(player.inventory));
+            }
+
+            return true;
+        }
+        else {
+            return false;
+        }
     }
 
     public SmallInventory getInventory() {
